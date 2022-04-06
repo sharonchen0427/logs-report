5b5ebb6b225b1042f4bd16bef4b5d696398f6a5d
-chenshumei <-chensharon0427@gmail.com>
2022-04-05 22:07:29 -0400

    Form use refs to fetch user input

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -1,11 +1,50 @@
+import {useRef, useState} from "react";
+
 function Login() {
+    const usernameInputRef = useRef()
+    const passwordInputRef = useRef()
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleUserName = (e) => {
+        console.log(e.target.value)
+        setUsername(e.target.value)
+    }
+
+    const handlePassword = (e) => {
+        console.log(e.target.value)
+        setPassword(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(`/token?username=${username}&password=${password}`)
+    }
+
     return (
         <>
             <p>Login</p>
-            <form>
-                <input type="text" name="username" /><br/>
-                <input type="password" name="password" /><br/>
-                <input type="submit" name="submit"/>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    id="username"
+                    ref={usernameInputRef}
+                    value={username}
+                    onChange={handleUserName}
+                /><br/>
+                <input
+                    type="text"
+                    id="password"
+                    ref={passwordInputRef}
+                    value={password}
+                    onChange={handlePassword}
+                /><br/>
+                <button
+                    type="submit"
+                    style={{width: '100px'}}
+                >Submit
+                </button>
             </form>
         </>
     );
