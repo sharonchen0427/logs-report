01085c9cbb6d611ef1d78fa80f3a36d3b8d7a7c4
-chenshumei <-chensharon0427@gmail.com>
2022-04-05 22:29:25 -0400

    Add material ui table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from "./screens/login";
+import NutritionTable from "./components/nutritionTable";
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+        <Login/>
+        <NutritionTable />
     </div>
   );
 }
